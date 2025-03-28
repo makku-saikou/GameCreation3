73f12c7940a124d3ab4bce564a43960402754898
hmxs <hmxsqaq@gmail.com>
2025-03-29 00:55:14 +0800

    [feat] Trampoline

---
+++ Assets/Scripts/GamePlay/Item/Trampoline.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace GamePlay.Item
+{
+	public class Trampoline : MonoBehaviour
+	{
+		[SerializeField] private float bounceForce = 10f;
+
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.CompareTag("Player"))
+			{
+				var rb = other.GetComponent<Rigidbody2D>();
+				if (rb)
+				{
+					rb.velocity = new Vector2(rb.velocity.x, bounceForce);
+				}
+			}
+		}
+	}
+}
