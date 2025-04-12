2e948f0a943d5094bcd8efff4999369783bba3b4
hmxs <hmxsqaq@gmail.com>
2025-04-12 19:01:04 +0800

    [feat] breakableplatform + thorn

---
+++ Assets/Scripts/GamePlay/Item/Thorn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace GamePlay.Item
+{
+	public class Thorn : MonoBehaviour
+	{
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.CompareTag("Player"))
+			{
+				// player die
+			}
+		}
+	}
+}
