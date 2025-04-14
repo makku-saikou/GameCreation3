30cac824069b70826649eb1613e58bbadabe5328
hmxs <hmxsqaq@gmail.com>
2025-04-15 02:57:48 +0800

    [feat] update target block/point

---
+++ Assets/Scripts/GamePlay/Item/AutoBoxColliderResizer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace GamePlay.Item
+{
+	[RequireComponent(typeof(BoxCollider2D), typeof(SpriteRenderer))]
+	public class AutoBoxColliderResizer : MonoBehaviour
+	{
+		private SpriteRenderer _spriteRenderer;
+		private BoxCollider2D _boxCollider;
+
+		private void Start()
+		{
+			_spriteRenderer = GetComponent<SpriteRenderer>();
+			_boxCollider = GetComponent<BoxCollider2D>();
+			ColliderResize();
+		}
+
+		[Button]
+		private void ColliderResize()
+		{
+			_spriteRenderer ??= GetComponent<SpriteRenderer>();
+			_boxCollider ??= GetComponent<BoxCollider2D>();
+
+			if (_spriteRenderer == null || _boxCollider == null)
+			{
+				Debug.LogError("SpriteRenderer or BoxCollider2D not found!");
+				return;
+			}
+
+			var bounds = _spriteRenderer.bounds;
+			_boxCollider.size = bounds.size;
+		}
+	}
+}
