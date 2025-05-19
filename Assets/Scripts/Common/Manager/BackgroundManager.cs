46c978c0b095cda225b2ad29c62d9e11069b4039
hmxs <hmxsqaq@gmail.com>
2025-05-19 15:59:12 +0800

    [feat] parallax background plus

--- Assets/Scripts/Common/Manager/BackgroundManager.cs
+++ Assets/Scripts/Common/Manager/BackgroundManager.cs
@@ -13,10 +13,11 @@ namespace GamePlay.Manager
 		{
 			public SpriteRenderer background;
 			public Vector2 speed = Vector2.one;
-			[VerticalGroup("Loop")] public bool loopX;
-			[VerticalGroup("Loop")] public bool loopY;
-			[VerticalGroup("Info")] [ReadOnly] public Vector2 textureUnitSize;
+			public float scaleFactor = 1.0f;
 			[VerticalGroup("Info")] [ReadOnly] public bool isInitialized = false;
+
+			[HideInInspector] public Vector2 textureUnitSize;
+			[HideInInspector] public Vector2 initialSpriteSize;
 
 			public Transform Transform => background.transform;
 
@@ -28,6 +29,8 @@ namespace GamePlay.Manager
 					background.sprite.texture.width / background.sprite.pixelsPerUnit * Transform.localScale.x,
 					background.sprite.texture.height / background.sprite.pixelsPerUnit * Transform.localScale.y
 				);
+				var size = background.sprite.bounds.size;
+				initialSpriteSize = new Vector2(size.x * Transform.localScale.x, size.y * Transform.localScale.y);
 				isInitialized = true;
 			}
 		}
@@ -38,6 +41,7 @@ namespace GamePlay.Manager
 		private List<Background> backgrounds = new();
 
 		[SerializeField] private Transform cameraTransform;
+		[SerializeField] private Camera mainCamera;
 		private Vector3 _lastCameraPosition;
 
 		private void Start()
@@ -63,21 +67,21 @@ namespace GamePlay.Manager
 			var delta = new Vector2(
 				cameraTransform.position.x - _lastCameraPosition.x,
 				cameraTransform.position.y - _lastCameraPosition.y);
+
+			var cameraHeight = mainCamera.orthographicSize * 2;
+			var cameraWidth = cameraHeight * mainCamera.aspect;
+
 			foreach (var bg in backgrounds.Where(bg => bg.isInitialized))
 			{
 				bg.Transform.position += new Vector3(delta.x * bg.speed.x, delta.y * bg.speed.y, 0);
 
-				if (bg.loopX && Mathf.Abs(cameraTransform.position.x - bg.Transform.position.x) > bg.textureUnitSize.x)
-				{
-					var offsetX = (cameraTransform.position.x - bg.Transform.position.x);
-					bg.Transform.position = new Vector3(cameraTransform.position.x + offsetX, bg.Transform.position.y, 0);
-				}
-				if (bg.loopY && Mathf.Abs(cameraTransform.position.y - bg.Transform.position.y) > bg.textureUnitSize.y)
-				{
-					var offsetY = (cameraTransform.position.y - bg.Transform.position.y) % bg.textureUnitSize.y;
-					bg.Transform.position = new Vector3(bg.Transform.position.x, cameraTransform.position.y + offsetY, 0);
-				}
+				float scaleRatioX = cameraWidth / bg.initialSpriteSize.x;
+				float scaleRatioY = cameraHeight / bg.initialSpriteSize.y;
+				float targetScale = Mathf.Max(scaleRatioX, scaleRatioY) * bg.scaleFactor;
+				bg.Transform.localScale = new Vector3(targetScale, targetScale, 1);
 			}
+
+
 		}
 	}
 }
