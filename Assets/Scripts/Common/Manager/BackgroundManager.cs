c58acd2bc7e7f434a29466ef404f9b90f1434d57
hmxs <hmxsqaq@gmail.com>
2025-06-02 15:54:23 +0800

    [feat] update bg

--- Assets/Scripts/Common/Manager/BackgroundManager.cs
+++ Assets/Scripts/Common/Manager/BackgroundManager.cs
@@ -13,24 +13,13 @@ namespace GamePlay.Manager
 		{
 			public SpriteRenderer background;
 			public Vector2 speed = Vector2.one;
-			public float scaleFactor = 1.0f;
 			[VerticalGroup("Info")] [ReadOnly] public bool isInitialized = false;
-
-			[HideInInspector] public Vector2 textureUnitSize;
-			[HideInInspector] public Vector2 initialSpriteSize;
 
 			public Transform Transform => background.transform;
 
 			public void Init()
 			{
 				if (!background || !background.sprite || !background.sprite.texture) return;
-
-				textureUnitSize = new Vector2(
-					background.sprite.texture.width / background.sprite.pixelsPerUnit * Transform.localScale.x,
-					background.sprite.texture.height / background.sprite.pixelsPerUnit * Transform.localScale.y
-				);
-				var size = background.sprite.bounds.size;
-				initialSpriteSize = new Vector2(size.x / Transform.localScale.x, size.y / Transform.localScale.y);
 				isInitialized = true;
 			}
 		}
@@ -68,20 +57,10 @@ namespace GamePlay.Manager
 				cameraTransform.position.x - _lastCameraPosition.x,
 				cameraTransform.position.y - _lastCameraPosition.y);
 
-			var cameraHeight = mainCamera.orthographicSize * 2;
-			var cameraWidth = cameraHeight * mainCamera.aspect;
-
 			foreach (var bg in backgrounds.Where(bg => bg.isInitialized))
 			{
 				bg.Transform.position += new Vector3(delta.x * bg.speed.x, delta.y * bg.speed.y, 0);
-
-				float scaleRatioX = cameraWidth / bg.initialSpriteSize.x;
-				float scaleRatioY = cameraHeight / bg.initialSpriteSize.y;
-				float targetScale = Mathf.Max(scaleRatioX, scaleRatioY) * bg.scaleFactor;
-				bg.Transform.localScale = new Vector3(targetScale, targetScale, 1);
 			}
-
-
 		}
 	}
 }
