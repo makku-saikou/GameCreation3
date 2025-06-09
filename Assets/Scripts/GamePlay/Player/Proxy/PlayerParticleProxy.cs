b46dc5df8f4210dbcd07d21932012cd53f4c6511
紫地丁 <hanfe666@outlook.com>
2025-06-09 14:45:10 +0800

    [fix] 玩家预制体

---
+++ Assets/Scripts/GamePlay/Player/Proxy/PlayerParticleProxy.cs
@@ -0,0 +1,19 @@
+// -------------------------------------------------
+// Copyright@ makku-saikou
+// Author : jianhao li
+// Date: 2025_06_08
+// Description:
+// -------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GamePlay.Player
+{
+    public class PlayerParticleProxy : MonoBehaviour
+    {
+        [SerializeField] List<ParticleSystem> m_ParticleSystems;
+        
+    }
+}
