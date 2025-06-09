09bb623a6e8828d182e005a85c2ca279cc59765b
hmxs <hmxsqaq@gmail.com>
2025-06-09 22:41:33 +0800

    [res] reconstruct anim folder

---
+++ Assets/Scripts/GamePlay/Item/Key.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace GamePlay.Item
+{
+	public class Key : MonoBehaviour
+	{
+		[SerializeField] [ReadOnly] private bool isCollected;
+
+		private void Start()
+		{
+
+		}
+	}
+}
