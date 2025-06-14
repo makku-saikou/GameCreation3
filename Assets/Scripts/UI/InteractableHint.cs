4aacc997d96c8845b2027ecd79a87ad715098074
hmxs <hmxsqaq@gmail.com>
2025-06-14 18:36:37 +0800

    [feat] E

---
+++ Assets/Scripts/UI/InteractableHint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI
+{
+	public class InteractableHint : MonoBehaviour
+	{
+		[SerializeField] private GameObject hintObject;
+
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.CompareTag("Player")) hintObject.SetActive(true);
+		}
+
+		private void OnTriggerExit2D(Collider2D other)
+		{
+			if (other.CompareTag("Player")) hintObject.SetActive(false);
+		}
+	}
+}
