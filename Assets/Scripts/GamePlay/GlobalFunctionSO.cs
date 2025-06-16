717a35a2010814590bbd2471abd8d19056db0896
hmxs <hmxsqaq@gmail.com>
2025-06-16 20:56:23 +0800

    [feat] disable player input in dialog

---
+++ Assets/Scripts/GamePlay/GlobalFunctionSO.cs
@@ -0,0 +1,21 @@
+﻿using Common.Manager;
+using UnityEngine;
+
+namespace GamePlay
+{
+	[CreateAssetMenu(fileName = "GlobalFunctionSO", menuName = "ScriptableObjects/GlobalFunctionSO", order = 1)]
+	public class GlobalFunctionSO : ScriptableObject
+	{
+		public void DisablePlayerInput()
+		{
+			var player = GameManager.Instance.Player;
+			if (player) player.Input.CanInput = false;
+		}
+
+		public void EnablePlayerInput()
+		{
+			var player = GameManager.Instance.Player;
+			if (player) player.Input.CanInput = true;
+		}
+	}
+}
