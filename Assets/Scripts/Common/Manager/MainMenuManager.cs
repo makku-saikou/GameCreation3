bd5efc5d52078eb6b13da86c43d7095ba8285d2a
紫地丁 <hanfe666@outlook.com>
2025-06-18 21:16:50 +0800

    [feat] 指令

--- Assets/Scripts/Common/Manager/MainMenuManager.cs
+++ Assets/Scripts/Common/Manager/MainMenuManager.cs
@@ -21,7 +21,52 @@ namespace Common.Manager
         private void Start()
         {
             AudioSystem.PlayBGM(bgm);
-
+            DebugSystem.AddCommand("Player/Die", () =>
+            {
+                GameManager.Instance.PlayerDie();
+            });
+            DebugSystem.AddCommand("Scene/MainMenu", () =>
+            {
+                UIManager.Instance.FadeOut(() =>
+                {
+                    SceneSystem.LoadScene(0);
+                });
+            });
+            DebugSystem.AddCommand("Scene/Level-0", () =>
+            {
+                UIManager.Instance.FadeOut(() =>
+                {
+                    SceneSystem.LoadScene(1);
+                });
+            });
+            DebugSystem.AddCommand("Scene/Level-1-1", () =>
+            {
+                UIManager.Instance.FadeOut(() =>
+                {
+                    SceneSystem.LoadScene(2);
+                });
+            });
+            DebugSystem.AddCommand("Scene/Level-1-2", () =>
+            {
+                UIManager.Instance.FadeOut(() =>
+                {
+                    SceneSystem.LoadScene(3);
+                });
+            });
+            DebugSystem.AddCommand("Scene/Level-1-3", () =>
+            {
+                UIManager.Instance.FadeOut(() =>
+                {
+                    SceneSystem.LoadScene(4);
+                });
+            });
+            DebugSystem.AddCommand("Scene/Level-1-4", () =>
+            {
+                UIManager.Instance.FadeOut(() =>
+                {
+                    SceneSystem.LoadScene(5);
+                });
+            });
         }
 
         public void Play()
